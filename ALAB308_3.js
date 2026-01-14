8edd594e88cc4dc344a095802297ba8bbdd4f8fe
chikeozuzu <chikeozuzu@gmail.com>
2026-01-13 20:25:41 -0500

    Began working on Part 1 of the ALAB308_3 Lab

---
+++ ALAB308_3.js
@@ -0,0 +1,9 @@
+//Lab ALAB 308.3.1
+
+/*  The task is to create a program that advises a group of environmental scientists on how to 
+    handle the growth and spread of a unique plant species under their supervision. 
+    To develop a growth control system that will monitor and predict the plant growth, 
+    making decisions based on the available space and potential growth.
+*/
+
+//  Part 1
