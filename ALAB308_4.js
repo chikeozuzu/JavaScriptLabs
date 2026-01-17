f8b0bcb7dcd4706d67d60a8dc5429ee861723605
chikeozuzu <chikeozuzu@gmail.com>
2026-01-17 13:47:32 -0500

    Began working on Part 1 of the ALAB308_4 Lab

---
+++ ALAB308_4.js
@@ -0,0 +1,12 @@
+//Lab ALAB 308.4.1
+
+/*  The task is to create two different data processing algorithms. 
+    The first stage will transform raw data into a formatted array of objects according to a specification. 
+    The second stage will use the output from the first stage to provide a textual report on the data.
+*/
+
+//  Part 1
+
+//  Refactoring the script from ALAB 308.3.1 that parsed CSVs. 
+//  With the knowledge of arrays and objects, how would you change your approach to this problem?
+
