1290aff1d9625f49b1b6c1f666a898998e8e60af
chikeozuzu <chikeozuzu@gmail.com>
2026-01-20 19:30:37 -0500

    Began working on Part 3 of the ALAB308_4 Lab

--- ALAB308_4.js
+++ ALAB308_4.js
@@ -53,3 +53,24 @@ console.log(twoDimensionalArray);
 //  Each row is an array, and all rows are contained within the parent array.
 //  The numberOfColumns variable dynamically reflects the number of columns based on the header row.
 //  This setup allows for flexible handling of CSV data with varying numbers of columns.
+
+//  Part 3
+
+/*  In order to make it more obvious what the data is, we will transform our rows into objects.
+    For each row of data in the result array produced by your code above, 
+    create an object where the key of each value is the heading for that value’s column.
+    Convert these keys to all lowercase letters for consistency.
+    Store these objects in an array, in the order that they were originally listed.
+    Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
+*/
+const headerRow = twoDimensionalArray[0];
+const objectArray = [];
+for (let i = 1; i < twoDimensionalArray.length; i++) {
+    const row = twoDimensionalArray[i];
+    const obj = {};
+    for (let j = 0; j < headerRow.length; j++) {
+        obj[headerRow[j].toLowerCase()] = row[j];
+    }
+    objectArray.push(obj);
+}
+console.log(objectArray);
